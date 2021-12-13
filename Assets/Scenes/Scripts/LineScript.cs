1c1f36e30c2af10496ad05c96713fba63c80bbc8
Catalin Enache <catalinenache78@gmail.com>
2021-12-13 09:18:02 +0200

    update

---
+++ Assets/Scenes/Scripts/LineScript.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class LineScript : MonoBehaviour
+{
+    public Vector3 p1Position = Vector3.zero;
+    public Vector3 p2Position = Vector3.up;
+    public float lineThickness = 0.01f;
+    public float handlersThickness = 0.1f;
+    private GameObject _p1;
+    private GameObject _p2;
+    private GameObject line;
+    void Start()
+    {
+        _p1 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        _p1.transform.SetPositionAndRotation(p1Position, Quaternion.identity);
+        _p1.transform.localScale = Vector3.one * handlersThickness;
+        _p1.AddComponent<Move3D>();
+        _p1.GetComponent<Move3D>().onChangeCallback += UpdateP1Position;
+        // _p1.GetComponent<Move3D>().onChangeEvent.AddListener(UpdateP1Position);
+
+        _p2 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        _p2.transform.SetPositionAndRotation(p2Position, Quaternion.identity);
+        _p2.transform.localScale = Vector3.one * handlersThickness;
+        _p2.AddComponent<Move3D>();
+        // _p2.GetComponent<Move3D>().onChangeCallback += UpdateP2Position;
+        _p2.GetComponent<Move3D>().onChangeEvent.AddListener(UpdateP2Position);
+
+        line = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        line.transform.SetPositionAndRotation(p1Position, Quaternion.identity);
+        line.transform.up = (p2Position - p1Position);
+        line.transform.localScale = Vector3.one * lineThickness;
+    }
+
+    private void OnDestroy()
+    {
+        new List<GameObject>(new []{_p1, _p2, line}).ForEach(Destroy);
+    }
+
+    void UpdateP1Position(GameObject p1)
+    {
+        p1Position = p1.transform.position;
+    }
+    
+    void UpdateP2Position(GameObject p2)
+    {
+        p2Position = p2.transform.position;
+    }
+
+    void Update()
+    {
+        // Sync with Inspector if positions were changed there.
+        _p1.transform.position = p1Position;
+        _p2.transform.position = p2Position;
+        
+        Vector3 diff = (p2Position - p1Position);
+        Vector3 orientation = diff.normalized;
+        float distance = diff.magnitude;
+        float lineSize = distance / 2; // cylinders have 2 units on y coord => we're making it 1 unit
+        float halfDistance = distance / 2; // related to middle point
+        Vector3 middlePoint = (p1Position + p2Position) / 2f;
+        
+        // scale
+        Vector3 localScale = line.transform.localScale;
+        line.transform.localScale = new Vector3(localScale.x,lineSize, localScale.z);
+        
+        // position
+        // line.transform.position = p1Position + orientation * halfDistance;
+        line.transform.position = middlePoint;
+        
+        // rotation
+        // line.transform.rotation = Quaternion.LookRotation(orientation);
+        // then
+        // line.transform.rotation *= Quaternion.Euler(90, 0, 0);
+        // or
+        // line.transform.rotation *= Quaternion.FromToRotation(Vector3.up, Vector3.forward);
+        // or just
+        line.transform.up = orientation;
+    }
+}
---
+++ Assets/Scenes/Scripts/Move3D.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ChangeEvent : UnityEvent<GameObject> { };
+
+[RequireComponent(typeof(Collider))]
+public class Move3D : MonoBehaviour
+{
+    private Camera mainCamera;
+    private float cameraZDistance;
+    private Boolean isDragging;
+    private Vector3 screenPosition;
+    private Vector3 newWorldPosition;
+    
+    public ChangeEvent onChangeEvent = new ChangeEvent();
+    
+    public delegate void OnChangeCallback(GameObject gameObject);
+    public event OnChangeCallback onChangeCallback;
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+ 
+    private void OnMouseDown()
+    {
+        isDragging = true;
+        cameraZDistance = mainCamera.WorldToScreenPoint(transform.position).z;
+    }
+
+    private void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+    void OnMouseDrag()
+    {
+        screenPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, cameraZDistance);
+        newWorldPosition = mainCamera.ScreenToWorldPoint(screenPosition);
+        transform.position = newWorldPosition;
+        onChangeCallback?.Invoke(gameObject);
+        onChangeEvent.Invoke(gameObject);
+    }
+
+    void OnGUI()
+    {
+        if (!isDragging) return;
+        Rect rect = new Rect(20, 20, mainCamera.pixelWidth - 40, mainCamera.pixelHeight - 40);
+        GUILayout.BeginArea(rect);
+        GUILayout.Label("Screen pixels: " + mainCamera.pixelWidth + ":" + mainCamera.pixelHeight);
+        GUILayout.Label("Screen position: " + screenPosition);
+        GUILayout.Label("World position: " + transform.position.ToString("F3"));
+        GUILayout.EndArea();
+        // EditorGUI.DrawRect(rect, new Color(1,1,0,0.05f));
+    }
+}
