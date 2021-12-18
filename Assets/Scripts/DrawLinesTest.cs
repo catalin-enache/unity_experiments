e46a7a5518640ae6caa823ffec226fe1a6484120
Catalin Enache <catalinenache78@gmail.com>
2021-12-18 10:35:33 +0200

    added namespaces

---
+++ Assets/Scripts/DrawLinesTest.cs
@@ -0,0 +1,35 @@
+﻿using Experiments.Lib;
+using UnityEngine;
+
+namespace Experiments
+{
+    public class DrawLinesTest : MonoBehaviour
+    {
+        private LinesDrawer ls;
+        void Start() 
+        {
+            GameObject obj = new GameObject("Lines Drawer");
+            ls = obj.AddComponent<LinesDrawer>();
+            ls.userListOfPointList.Add(new UserPointList());
+            ls.userListOfPointList[0].Add(new Vector3(0,0,0));
+            ls.userListOfPointList[0].Add(new Vector3(1,1,0));
+            ls.userListOfPointList.Add(new UserPointList());
+            ls.userListOfPointList[1].Add(new Vector3(2,2,0));
+            ls.userListOfPointList[1].Add(new Vector3(3,3,0));
+            ls.userListOfPointList[1].Add(new Vector3(4,4,0));
+
+            ls.onChangeCallback += OnChange;
+        }
+
+        void Update()
+        {
+            // Vector3 tmp = ls.userListOfPointList[0][0];
+            // ls.userListOfPointList[0][0] = new Vector3(tmp.x + Time.deltaTime * 1, tmp.y);
+        }
+
+        void OnChange(Move3D point)
+        {
+            // Debug.Log(point.name);
+        }
+    }
+}
---
+++ Assets/Scripts/Lib/Move3D.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Experiments.Lib
+{
+    public class Move3DMovingEvent : UnityEvent<Move3D> { };
+
+    [RequireComponent(typeof(Collider))]
+    public class Move3D : MonoBehaviour
+    {
+        private static Color hoverColor = Color.red;
+        private Camera mainCamera;
+        private float cameraZDistance;
+        private Boolean isDragging;
+        private Vector3 screenPosition;
+        private Vector3 newWorldPosition;
+        public Dictionary<string, object> metaData = new Dictionary<string, object>();
+        public Move3DMovingEvent OnMove3DMovingEvent = new Move3DMovingEvent();
+        private Color initialColor;
+    
+        void Start()
+        {
+            mainCamera = Camera.main;
+            initialColor = GetComponent<Renderer>().material.color;
+        }
+ 
+        private void OnMouseDown()
+        {
+            isDragging = true;
+            cameraZDistance = mainCamera.WorldToScreenPoint(transform.position).z;
+        }
+
+        private void OnMouseUp()
+        {
+            isDragging = false;
+        }
+
+        private void OnMouseOver()
+        {
+            GetComponent<Renderer>().material.color = hoverColor;
+        }
+
+        private void OnMouseExit()
+        {
+            GetComponent<Renderer>().material.color = initialColor;
+        }
+
+        void OnMouseDrag()
+        {
+            screenPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, cameraZDistance);
+            newWorldPosition = mainCamera.ScreenToWorldPoint(screenPosition);
+            transform.position = newWorldPosition;
+            OnMove3DMovingEvent.Invoke(this);
+        }
+
+        void OnGUI()
+        {
+            if (!isDragging) return;
+            Rect rect = new Rect(20, 20, mainCamera.pixelWidth - 40, mainCamera.pixelHeight - 40);
+            GUILayout.BeginArea(rect);
+            GUILayout.Label("Name: " + name);
+            GUILayout.Label("Screen pixels: " + mainCamera.pixelWidth + ":" + mainCamera.pixelHeight);
+            GUILayout.Label("Screen position: " + screenPosition);
+            GUILayout.Label("World position: " + transform.position.ToString("F3"));
+            GUILayout.EndArea();
+            // EditorGUI.DrawRect(rect, new Color(1,1,0,0.05f));
+        }
+    }
+}
